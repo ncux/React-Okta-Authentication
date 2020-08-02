764e860be778c689087823c085149f9d88d5e3c1
Talent Malaba <malabatalent@gmail.com>
2020-08-02 12:02:08 +0800

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/bootstrap4-daydream.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
